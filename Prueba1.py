7886fde4926cc41b0ff5a8a137f0175a24ecd317
MiguelAngelGitHu <73709296+MiguelAngelGitHu@users.noreply.github.com>
2020-10-30 17:04:27 +0100

    Add files via upload

---
+++ Prueba1.py
@@ -0,0 +1,6 @@
+Character_name = "Jhon"
+Character_age = "35"
+print("There once was a man named " + Character_name + ", ")
+print("he was " + Character_age + " years old. ")
+print("He really liked the name " + Character_name + ", ")
+print("but didn't like being " + Character_age + ".")
